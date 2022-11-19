4310b519c962f13ff84a9a4ea3dbb549435a1afa
RaghavRamesh159 <raghavendranr.1504@gmail.com>
2022-11-18 22:23:41 -0800

    Cleaned up code, have to write code to register faces from db automatically

--- start.py
+++ start.py
@@ -24,82 +24,7 @@ def main():
     screen_size = (800, 600)
     scale = (1, 1)
 
-    frame = cv2.imread("./group.jpg")
-    
-
-
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument(
-    #     "-db",
-    #     "--db_path",
-    #     help="Database path . Use relative path . Default path : " + db_path,
-    # )
-    # parser.add_argument(
-    #     "-smp",
-    #     "--siamese_model_path",
-    #     help="Siamese Model path . Use relative path . Default path : "
-    #     + siamese_model_path,
-    # )
-    # parser.add_argument(
-    #     "-load",
-    #     "--load_from_file",
-    #     help="[TRUE] if you want to load reference embeddings from previously generated file , [FALSE] if you want to recompile or create new embeddings for the reference images . Default is set to TRUE",
-    # )
-    # parser.add_argument(
-    #     "-yolov5",
-    #     "--yolov5_type",
-    #     help="Enter which yolov5 model you want to use : [yolov5s] ,[yolov5m] ,[yolov5l] ,[yolov5x] . Default type : yolov5m ",
-    # )
-    # parser.add_argument(
-    #     "-cdl",
-    #     "--cooldown_limit",
-    #     help=f" Lower the cooldown higher the precision higher the memory usage . Default value : {cooldown_limit}s",
-    # )
-    # parser.add_argument(
-    #     "-rcl",
-    #     "--regular_check_limit",
-    #     help=f" Helps in correcting previous errors by either the camera or the program . Default value : {regular_check_limit}s",
-    # )
-    # parser.add_argument(
-    #     "-size",
-    #     "--screen_size",
-    #     help=f"Set Default screen size for the webcam feed : [SCREEN_W*SCREEN_H] . Default size : {screen_size[0]}*{screen_size[1]} ",
-    # )
-    # parser.add_argument(
-    #     "-scale",
-    #     "--scale",
-    #     help=f"Set Default scale for the webcam feed : [SCALE_X*SCALE_Y] . Default size : {scale[0]}*{scale[1]} ",
-    # )
-
-    # args = parser.parse_args()
-    # if args.db_path:
-    #     db_path = args.db_path
-
-    # if args.siamese_model_path:
-    #     siamese_model_path = args.siamese_model_path
-
-    # if args.load_from_file:
-    #     if args.load_from_file.upper() == "FALSE":
-    #         load_from_file = False
-
-    # if args.yolov5_type:
-    #     yolov5_type = args.yolov5_type
-
-    # if args.cooldown_limit:
-    #     cooldown_limit = float(args.cooldown_limit)
-
-    # if args.regular_check_limit:
-    #     regular_check_limit = float(args.regular_check_limit)
-
-    # if args.screen_size:
-    #     screen_size = []
-    #     for i in args.screen_size.split("*"):
-    #         screen_size.append(int(i))
-
-    # if args.scale:
-    #     scale = []
-    #     for i in args.scale.split("*"):
-    #         scale.append(int(i))
+    frame = cv2.imread("./llt.jpg")
 
     # Initializing all the models and reference images
     device, classes, loader, reference_cropped_img, yolov5, resnet, mtcnn, model = init(
@@ -109,21 +34,12 @@ def main():
         yolov5_type=yolov5_type,
     )
 
+    face_boxes = []  # selecting person class alone
+    face_name = []  # selecting person class alone
+
     boxes, probs, points = mtcnn.detect(frame[:, :, ::-1], landmarks=True)
-    print("boxes", boxes[0])
-    x_min, y_min, x_max, y_max = boxes[0]
-    x_min, y_min, x_max, y_max = int(x_min), int(y_min), int(x_max), int(y_max)
-    # cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
-    # cv2.imshow("frame", frame)
-    person_names = []
-    face_boxes = []
-    face_name = []
-
-
     if boxes is not None:
         for box in boxes:  # classifying predicted boxes
-            x_min, y_min, x_max, y_max = box
-            x_min, y_min, x_max, y_max = int(x_min), int(y_min), int(x_max), int(y_max)
             predicted_class, similarity = classify(
                 box,
                 frame,
@@ -139,171 +55,35 @@ def main():
                 face_name.append("Stranger")
             else:
                 face_name.append(predicted_class)
-            frame = cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (255, 0 ,0), 2)
-            cv2.putText(
-                frame,
-                f"{face_name[-1]}",
-                (x_min, y_min - 10),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.6,
-                (255, 0, 0),
-                2,
-            )
-    cv2.imshow("faces", frame)
 
-        
-    print(face_name)
+    for i, j in zip(face_boxes, face_name):
+        x_min, y_min, x_max, y_max = i
+        x_min, y_min, x_max, y_max = int(x_min), int(y_min), int(x_max), int(y_max)
+        # color coding boxes
+        color = (0, 255, 0)  # Green
+        if j == "Stranger":
+            color = (0, 0, 255)  # Red
 
-    # Initializing cooldown clocks and Face-Recognition paramaters
-    n_people = 0  # Number of people confirmed after cooldown
-    cooldown = 0
-    new_frame_time = 0
-    prev_frame_time = 0
-    regular_check_cooldown = 0
-
-    classify_faces = True  # Wheather to use MTCNN to classify faces
-    start_cooldown = False  # Starts when there is change in number of people
-    person_names = []
-
-    #cap = cv2.VideoCapture(0)
-
-    # if not cap.isOpened():
-    #     raise IOError("Cannot open webcam")
-
-    while True:
-        #ret, frame = cap.read()
-        #new_frame_time = time.time()
-        # time_diff = new_frame_time - prev_frame_time
-        # fps = int(1 / (time_diff))
-        # regular_check_cooldown = regular_check_cooldown + time_diff
-        #fps = cap.get(cv2.CAP_PROP_FPS)
-        frame = cv2.imread("./group.jpg")
-        boxes_info = yolov5(frame).xyxy[0].cpu().numpy().tolist()
-        person_boxes = []  # selecting person class alone
-
-        ith_n_people = 0  # number of person in frame at the moment (It might be wrong and is confirmed through cooldown)
-        for i in boxes_info:
-            if i[5] == 0:  # class for person is 0
-                person_boxes.append(tuple(i[:4]))
-                ith_n_people = ith_n_people + 1
-
-        if ith_n_people != n_people and start_cooldown == False:
-            start_cooldown = True
-
-        if ith_n_people == n_people:
-            cooldown = 0
-            start_cooldown = False
-
-        if regular_check_cooldown >= regular_check_limit:
-            regular_check_cooldown = 0
-            classify_faces = True
-
-        # if start_cooldown:
-        #     cooldown = cooldown + time_diff
-
-        if (
-            cooldown >= cooldown_limit
-        ):  # Confirming if the change in number is slight error
-            n_people = ith_n_people
-            cooldown = 0
-            start_cooldown = False
-            classify_faces = True  # Number of people in frame is changed so we feed the frame into MTCNN
-
-        if (
-            classify_faces and n_people == 0
-        ):  # If number of peopel is 0 in frame then there is no need to classify
-            person_names = []
-            face_boxes = []
-            face_name = []
-            classify_faces = False
-
-        if classify_faces:
-            # Initializing new boxes and person name
-            person_names = []
-            face_boxes = []
-            face_name = []
-
-            classify_faces = False
-            boxes, probs, points = mtcnn.detect(frame[:, :, ::-1], landmarks=True)
-
-            if boxes is not None:
-                for box in boxes:  # classifying predicted boxes
-                    predicted_class, similarity = classify(
-                        box,
-                        frame,
-                        loader,
-                        resnet,
-                        model,
-                        reference_cropped_img,
-                        classes,
-                        device,
-                    )
-                    face_boxes.append(box)
-                    if predicted_class == -1:
-                        face_name.append("Stranger")
-                    else:
-                        face_name.append(predicted_class)
-
-            for i in person_boxes:
-                temp_name = ""
-                new_max = 0
-                for j, k in zip(face_boxes, face_name):
-                    iou = IOU(
-                        i, j, screen_size=tuple(frame.shape[:2])
-                    )  # The box for the person and box for the person's face must intersect the highest
-                    if new_max < iou:
-                        new_max = iou
-                        temp_name = k
-
-                person_names.append(temp_name)
-
-        check_repeat = (
-            []
-        )  # Make faces of same person cannot present at the same time (prevent error due to yolov5 smaller models)
-        for i, j in zip(person_boxes, person_names):
-            if j in check_repeat:
-                continue
-            check_repeat.append(j)
-            x_min, y_min, x_max, y_max = i
-            x_min, y_min, x_max, y_max = int(x_min), int(y_min), int(x_max), int(y_max)
-
-            # color coding boxes
-
-            color = (0, 255, 0)  # Green
-            if j == "Stranger":
-                check_repeat.pop()
-                color = (0, 0, 255)  # Red
-
-            frame = cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (color), 2)
-            cv2.putText(
-                frame,
-                f"{j}",
-                (x_min, y_min - 10),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.6,
-                (255, 0, 0),
-                2,
-            )
-
-        # changing frame size
-
-        frame = cv2.resize(
+        frame = cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (color), 2)
+        cv2.putText(
             frame,
-            tuple(screen_size),
-            fx=scale[0],
-            fy=scale[1],
-            interpolation=cv2.INTER_AREA,
+            f"{j}",
+            (x_min, y_min - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.6,
+            (255, 255, 255),
+            2,
         )
-
-        prev_frame_time = new_frame_time
-        # cv2.putText(
-        #     frame, f"FPS:{fps}", (15, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2
-        # )
-        #cv2.imshow("WebCam", frame)
-
-        c = cv2.waitKey(1)  # User input
-        if c == 27:  # ASCII for ESC
-            break
+    print("here")
+    scale_percent = 30# percent of original size
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    
+    frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+    cv2.imshow("frame",frame)
+    cv2.imwrite(frame, "output.jpeg")
+    cv2.waitKey(0) 
 
     #cap.release()
     cv2.destroyAllWindows()
@@ -401,16 +181,26 @@ def init(
 
         
         classes.append("Friend1")
-        classes.append("Friend2")
+        classes.append("Raghav")
+        classes.append("Shru")
+        classes.append("Suk")
+
 
        
         reference_img.append(
                 Image.open(db_path + "/Friend1/nads.jpeg" )
             )
         reference_img.append(
-                Image.open(db_path + "/Friend2/IMG_20221115_173116.jpg" )
+                Image.open(db_path + "/Raghav/IMG_20221115_173116.jpg" )
+            )
+        reference_img.append(
+                Image.open(db_path + "/Shru/shru.jpg" )
             )
         
+        reference_img.append(
+                Image.open(db_path + "/Suk/suk.jpg" )
+            )
+                
         print("reference_img", reference_img)
 
         print("Creating new embeddings for the reference images.....")
